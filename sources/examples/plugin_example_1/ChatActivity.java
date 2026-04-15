5e423b3480d3840fa5b2fd28eb620bea78a31544
Ne_Xomyahok <149205245+Hshdhr1@users.noreply.github.com>
2026-04-16 00:53:19 +0300

    Build Material You styled AI chat mock screen

--- sources/examples/plugin_example_1/ChatActivity.java
+++ sources/examples/plugin_example_1/ChatActivity.java
@@ -1,11 +1,24 @@
 package ru.yufic.exteraPlugins;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
 /* loaded from: /storage/emulated/0/Android/data/com.apktools.app.decompile/files/decompile_temp/jadx/classes64.dex */
 public class ChatActivity extends AppCompatActivity {
     private void a(Bundle bundle) {
+        ImageView imageView;
+        int identifier = getResources().getIdentifier("chat_back", "id", getPackageName());
+        if (identifier == 0 || (imageView = findViewById(identifier)) == null) {
+            return;
+        }
+        imageView.setOnClickListener(new View.OnClickListener() { // from class: ru.yufic.exteraPlugins.ChatActivity.1
+            @Override // android.view.View.OnClickListener
+            public void onClick(View view) {
+                ChatActivity.this.finish();
+            }
+        });
     }
 
     private void b() {
